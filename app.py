280fbb003ea128284b39cbeb014ca4436104254d
yellowBunny <zigggin3@gmail.com>
2022-09-26 12:10:41 +0200

    working withdraw method

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ class Account:
     def __init__(self, statting_account_state):
         self.account = statting_account_state
     def withdraw(self, money: int)->None:
-        return 
+        self.account -=  money
+        
     def printStatment(self)-> None:
         pass
     def deposit(self, money: int)->None:
         pass
+
