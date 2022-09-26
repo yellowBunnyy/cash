ea25bce5b9fbf9f803d88603afbe228add69aeef
yellowBunny <zigggin3@gmail.com>
2022-09-26 12:15:14 +0200

    test deposit money at bank

--- tests.py
+++ tests.py
@@ -3,3 +3,8 @@ def test_withdraw_money_from_account():
     account = Account(100)
     account.withdraw(100)
     assert account.account == 0
+
+def test_depisit_money_return_sum_depisit_and_money_in_acount():
+    account = Account(100)
+    account.deposit(100)
+    assert account.account == 200
