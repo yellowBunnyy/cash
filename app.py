3abc2a01bdffbaea7ccf83ba11e7027eee43d2a3
yellowBunny <zigggin3@gmail.com>
2022-09-27 11:20:04 +0200

    add test which test current date as str

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import datetime
+import pdb
+
 class Account:
     def __init__(self, statting_account_state):
         self.account = statting_account_state
@@ -7,6 +10,10 @@ class Account:
 
     def deposit(self, money: int) -> None:
         self.account += money
+    
+    def get_string_current_date(self):
+        return datetime.date.today()
 
     def printStatment(self) -> None:
         pass
+
--- tests.py
+++ tests.py
@@ -1,4 +1,10 @@
+import datetime
+from unittest.mock import patch
 from app import Account
+
+
+
+
 def test_withdraw_money_from_account():
     account = Account(100)
     account.withdraw(100)
@@ -8,3 +14,11 @@ def test_depisit_money_return_sum_depisit_and_money_in_acount():
     account = Account(100)
     account.deposit(100)
     assert account.account == 200
+
+@patch("app.datetime")
+def test_get_current_date(mock_date):
+    mock_date.date.today.return_value = datetime.date(2022, 1, 13)
+    inst = Account(100)
+    assert inst.get_string_current_date() == "13-01-2022"
+
+
